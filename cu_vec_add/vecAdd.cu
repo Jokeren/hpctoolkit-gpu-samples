28433a7ff234ab7a8366919603ce2e4763127542
Keren Zhou <robinho364@gmail.com>
2018-03-30 09:32:06 -0500

    Add cubin vec add

---
+++ cu_vec_add/vecAdd.cu
@@ -0,0 +1,8 @@
+extern "C"
+__global__
+void vecAdd(int *l, int *r, int *p, size_t N) {
+  size_t idx = blockDim.x * blockIdx.x + threadIdx.x;
+  if (idx < N) {
+    p[idx] = l[idx] + r[idx];
+  }
+}
