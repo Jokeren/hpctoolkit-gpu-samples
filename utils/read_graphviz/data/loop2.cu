3a771397d165edd87943943f3e64207e45bbb7fd
Jokeren <robinho364@gmail.com>
2018-05-19 02:39:23 -0500

    Update test samples

--- utils/read_graphviz/data/loop2.cu
+++ utils/read_graphviz/data/loop2.cu
@@ -1,8 +1,6 @@
 __global__
 void vecAdd(float *l, float *r, float *result, size_t N) {
   for (size_t i = 0; i < N; ++i) {
-    for (size_t j = 0; j < N; ++j) {
-      result[i * N + j] = l[i] + r[i];
-    }
+    result[i] = l[i] + r[i];
   }
 }
