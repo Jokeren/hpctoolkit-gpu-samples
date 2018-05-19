98cf466c075cb6688aca41258c0f4dcee5f8733b
Jokeren <robinho364@gmail.com>
2018-05-19 02:57:37 -0500

    Update test samples

--- utils/read_graphviz/data/loop3.cu
+++ utils/read_graphviz/data/loop3.cu
@@ -2,9 +2,13 @@ __global__
 void vecAdd(float *l, float *r, float *result, size_t N) {
   size_t i = threadIdx.x;
   if (l[i] > i) {
-    result[i] = l[i] - r[i];
+    goto LABEL1;
   } else {
-    result[i] = l[i] + r[i];
+    goto LABEL2;
   }
+LABEL1:
+  result[i] = l[i] - r[i];
+LABEL2:
+  result[i] = l[i] + r[i];
 }
 
