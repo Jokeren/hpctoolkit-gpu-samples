18fa6cbd78c5e2f63f70716ba46df72f69fd418e
Jokeren <robinho364@gmail.com>
2018-10-02 15:00:30 -0500

    Support arbitrary number of threads

--- cu_call_path/main.cu
+++ cu_call_path/main.cu
@@ -26,18 +26,6 @@ void output(int *p, size_t size) {
 }
 
 int main(int argc, char *argv[]) {
-  int l1[N], l2[N];
-  int r1[N], r2[N];
-  int p1[N], p2[N];
-  CUdeviceptr dl1, dl2;
-  CUdeviceptr dr1, dr2;
-  CUdeviceptr dp1, dp2;
-
-  init(l1, N);
-  init(r1, N);
-  init(l2, N);
-  init(r2, N);
-
 #ifdef USE_MPI
   int numtasks, rank;
   MPI_Init(&argc, &argv);
@@ -46,6 +34,7 @@ int main(int argc, char *argv[]) {
   printf("MPI task %d/%d\n", rank, numtasks);
 #endif
 
+  // Init device
   CUdevice device;
   CUcontext context;
   int device_id = 0;
@@ -56,63 +45,53 @@ int main(int argc, char *argv[]) {
 
   #pragma omp parallel
   {
+    int l[N], r[N], p[N];
+    CUdeviceptr dl, dr, dp;
+
+    init(l, N);
+    init(r, N);
+
     size_t threads = 256;
     size_t blocks = (N - 1) / threads + 1;
+
     DRIVER_API_CALL(cuCtxSetCurrent(context));
+
     CUmodule moduleAdd;
     CUfunction vecAdd;
-
     DRIVER_API_CALL(cuModuleLoad(&moduleAdd, "vecAdd.cubin"));
     DRIVER_API_CALL(cuModuleGetFunction(&vecAdd, moduleAdd, "vecAdd"));
 
-    if (omp_get_thread_num() == 0) {
-      DRIVER_API_CALL(cuMemAlloc(&dl1, N * sizeof(int)));
-      DRIVER_API_CALL(cuMemAlloc(&dr1, N * sizeof(int)));
-      DRIVER_API_CALL(cuMemAlloc(&dp1, N * sizeof(int)));
-      DRIVER_API_CALL(cuMemcpyHtoD(dl1, l1, N * sizeof(int))); 
-      DRIVER_API_CALL(cuMemcpyHtoD(dr1, r1, N * sizeof(int))); 
+    DRIVER_API_CALL(cuMemAlloc(&dl, N * sizeof(int)));
+    DRIVER_API_CALL(cuMemAlloc(&dr, N * sizeof(int)));
+    DRIVER_API_CALL(cuMemAlloc(&dp, N * sizeof(int)));
+    DRIVER_API_CALL(cuMemcpyHtoD(dl, l, N * sizeof(int))); 
+    DRIVER_API_CALL(cuMemcpyHtoD(dr, r, N * sizeof(int))); 
 
-      void *args[6] = {
-        &dl1, &dr1, &dp1, &N, &iter1, &iter2
-      };
+    void *args[6] = {
+      &dl, &dr, &dp, &N, &iter1, &iter2
+    };
 
-      GPU_TEST_FOR(DRIVER_API_CALL(cuLaunchKernel(vecAdd, blocks, 1, 1, threads, 1, 1, 0, 0, args, 0)));
+    GPU_TEST_FOR(DRIVER_API_CALL(cuLaunchKernel(vecAdd, blocks, 1, 1, threads, 1, 1, 0, 0, args, 0)));
 
-      DRIVER_API_CALL(cuMemcpyDtoH(l1, dl1, N * sizeof(int))); 
-      DRIVER_API_CALL(cuMemcpyDtoH(r1, dr1, N * sizeof(int))); 
-      DRIVER_API_CALL(cuMemcpyDtoH(p1, dp1, N * sizeof(int))); 
-      DRIVER_API_CALL(cuMemFree(dl1));
-      DRIVER_API_CALL(cuMemFree(dr1));
-      DRIVER_API_CALL(cuMemFree(dp1));
-    } else if (omp_get_thread_num() == 1) {
-      DRIVER_API_CALL(cuMemAlloc(&dl2, N * sizeof(int)));
-      DRIVER_API_CALL(cuMemAlloc(&dr2, N * sizeof(int)));
-      DRIVER_API_CALL(cuMemAlloc(&dp2, N * sizeof(int)));
-      DRIVER_API_CALL(cuMemcpyHtoD(dl2, l2, N * sizeof(int))); 
-      DRIVER_API_CALL(cuMemcpyHtoD(dr2, r2, N * sizeof(int))); 
+    DRIVER_API_CALL(cuMemcpyDtoH(l, dl, N * sizeof(int))); 
+    DRIVER_API_CALL(cuMemcpyDtoH(r, dr, N * sizeof(int))); 
+    DRIVER_API_CALL(cuMemcpyDtoH(p, dp, N * sizeof(int))); 
+    DRIVER_API_CALL(cuMemFree(dl));
+    DRIVER_API_CALL(cuMemFree(dr));
+    DRIVER_API_CALL(cuMemFree(dp));
 
-      void *args[6] = {
-        &dl2, &dr2, &dp2, &N, &iter1, &iter2
-      };
-
-      GPU_TEST_FOR(DRIVER_API_CALL(cuLaunchKernel(vecAdd, blocks, 1, 1, threads, 1, 1, 0, 0, args, 0)));
-
-      DRIVER_API_CALL(cuMemcpyDtoH(l2, dl2, N * sizeof(int))); 
-      DRIVER_API_CALL(cuMemcpyDtoH(r2, dr2, N * sizeof(int))); 
-      DRIVER_API_CALL(cuMemcpyDtoH(p2, dp2, N * sizeof(int))); 
-      DRIVER_API_CALL(cuMemFree(dl2));
-      DRIVER_API_CALL(cuMemFree(dr2));
-      DRIVER_API_CALL(cuMemFree(dp2));
-    }
     DRIVER_API_CALL(cuModuleUnload(moduleAdd));
+
+    #pragma omp critical
+    {
+      printf("Thread %d\n", omp_get_thread_num());
+      output(p, N);
+    }
   }
 
   cudaDeviceSynchronize();
   DRIVER_API_CALL(cuCtxSynchronize());
   DRIVER_API_CALL(cuCtxDestroy(context));
-
-  output(p1, N);
-  output(p2, N);
 
 #ifdef USE_MPI
   MPI_Finalize();
