16a333c4e4bb3b396e5c47c6d01cca11d30ec543
Jokeren <robinho364@gmail.com>
2019-02-12 16:55:45 -0500

    Confirm that samples are collected independently

---
+++ cuda_pc_sample_sm_check/main.cu
@@ -0,0 +1,93 @@
+#include <cstdio>
+#include <omp.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#ifdef USE_MPI
+#include <mpi.h>
+#endif
+
+#include "../utils/common.h"
+
+
+static const size_t N = 10000;
+static const size_t ITER = 200;
+
+
+void init(int *p, size_t size) {
+  for (size_t i = 0; i < size; ++i) {
+    p[i] = i;
+  }
+}
+
+
+void output(int *p, size_t size) {
+  for (size_t i = 0; i < size; ++i) {
+    printf("index %zu: %d\n", i, p[i]);
+  }
+}
+
+
+__global__
+void vecAdd(int *l, int *r, int *p, size_t N) {
+  size_t idx = blockDim.x * blockIdx.x + threadIdx.x;
+  if (idx < N) {
+    for (size_t i = 0; i < ITER; ++i) {
+      p[idx] = l[idx] + r[idx];
+    }
+  }
+}
+
+
+int main(int argc, char *argv[]) {
+#ifdef USE_MPI
+  int numtasks, rank;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  printf("MPI task %d/%d\n", rank, numtasks);
+#endif
+
+  // Init device
+  int device_id = 0;
+  if (argc > 1) {
+    device_id = atoi(argv[1]);
+  }
+  cuda_init_device(device_id);
+
+  #pragma omp parallel
+  {
+    int l[N], r[N], p[N];
+    int *dl, *dr, *dp;
+
+    init(l, N);
+    init(r, N);
+
+    RUNTIME_API_CALL(cudaMalloc(&dl, N * sizeof(int)));
+    RUNTIME_API_CALL(cudaMalloc(&dr, N * sizeof(int)));
+    RUNTIME_API_CALL(cudaMalloc(&dp, N * sizeof(int)));
+
+    RUNTIME_API_CALL(cudaMemcpy(dl, l, N * sizeof(int), cudaMemcpyHostToDevice));
+    RUNTIME_API_CALL(cudaMemcpy(dr, r, N * sizeof(int), cudaMemcpyHostToDevice));
+
+    size_t threads = 256;
+    size_t blocks = 4;
+
+    GPU_TEST_FOR((vecAdd<<<blocks, threads>>>(dl, dr, dp, N)));
+
+    blocks = 1;
+    GPU_TEST_FOR((vecAdd<<<blocks, threads>>>(dl, dr, dp, N)));
+
+    RUNTIME_API_CALL(cudaMemcpy(p, dp, N * sizeof(int), cudaMemcpyDeviceToHost));
+
+    RUNTIME_API_CALL(cudaFree(dl));
+    RUNTIME_API_CALL(cudaFree(dr));
+    RUNTIME_API_CALL(cudaFree(dp));
+  }
+
+  cudaDeviceSynchronize();
+
+#ifdef USE_MPI
+  MPI_Finalize();
+#endif
+  return 0;
+}
