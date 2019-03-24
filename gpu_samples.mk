5d080996b1ab4043c3946a251786e0665af5b7a4
Jokeren <robinho364@gmail.com>
2019-03-24 17:38:28 -0400

    Update makefiles

--- gpu_samples.mk
+++ gpu_samples.mk
@@ -1,8 +1,22 @@
 CXX ?= nvcc
 
-CPU_FLAGS := -g -fopenmp
+CPU_FLAGS += -g -fopenmp
 
-GPU_FLAGS := -lineinfo -arch $(ARCH)
+GPU_FLAGS += -lineinfo -arch $(ARCH)
+
+CUBIN_FLAGS += -cubin
+
+DEVICE_FLAGS += -dc
+
+LDFLAGS += -lcudart -lcuda -lstdc++ -lm
+
+ifneq ($(DEVICE),)
+DEVICE_OBJ := $(addprefix $(DEVICE), .o)
+endif
+
+ifneq ($(CUBIN),)
+CUBIN_CUBIN := $(addprefix $(CUBIN), .cubin)
+endif
 
 ifeq ($(CPU_ARCH), power8)
 	CPU_FLAGS += -mcpu=pwr8 -mtune=pwr8
@@ -18,17 +32,24 @@ endif
 
 CXXFLAGS := -Xcompiler "$(CPU_FLAGS)" $(GPU_FLAGS)
 
-all: exec cubin
+all: exec cubin device
 	
 exec: $(EXEC)
 
-$(EXEC): % : %.cu
-	$(CXX) $(LDFLAGS) -o $@ $< $(SHOW_FLAG) $(CXXFLAGS) -lcudart -lcuda -lstdc++ -lm
+$(EXEC): % : %.cu $(DEVICE_OBJ)
+	$(CXX) -o $@ $^ $(SHOW_FLAG) $(CXXFLAGS) $(LDFLAGS)
 
-cubin: $(CUBIN)
+cubin: $(CUBIN_CUBIN)
 
-$(CUBIN): % : %.cu
-	$(CXX) $(GPU_FLAGS) -cubin $<
+$(CUBIN_CUBIN): %.cubin : %.cu
+	$(CXX) $(GPU_FLAGS) $(CUBIN_FLAGS) $<
+
+device: $(DEVICE_OBJ)
+
+$(DEVICE_OBJ): %.o : %.cu
+	$(CXX) $(GPU_FLAGS) $(DEVICE_FLAGS) $<
 
 clean:
 	rm -rf $(EXEC) *.o *.dot *.hpcstruct *.cubin
+
+print-% : ; $(info $* is $(flavor $*) variable set to [$($*)]) @true
