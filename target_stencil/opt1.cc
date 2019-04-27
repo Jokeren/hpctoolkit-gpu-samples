04ae46a6a037d9b7b2ba46ad5d924c017e0cd2a4
Jokeren <robinho364@gmail.com>
2019-04-27 11:27:47 -0400

    Init tutorial example

---
+++ target_stencil/opt1.cc
@@ -0,0 +1,28 @@
+while (error > T && iter < ITER) {
+  error = 0.0;
+
+  #pragma omp target data map(A[:H * W], A_new[:H * W])
+  {
+    #pragma omp target parallel for reduction(max:error) map(error)
+    for (size_t h = 1; h < H - 1; h++) {
+      for (size_t w = 1; w < W - 1; w++) {
+        A_new[h * W + w] = 0.25 * (A[h * W + w + 1] + A[h * W + w - 1] +
+          A[(h + 1) * W + w] + A[(h - 1) * W + w]);
+        error = fmax(error, fabs(A_new[h * W + w] - A[h * W + w]));
+      } 
+    }
+  }
+
+  #pragma omp target data map(A[:H * W], A_new[:H * W])
+  {
+    #pragma omp target parallel for
+    for (size_t h = 1; h < H - 1; h++) {
+      for (size_t w = 1; w < W - 1; w++) {
+        A[h * W + w] = A_new[h * W + w];
+      }
+    }
+  }
+
+  printf("Phase %lu, error: %f\n", iter, error);
+  ++iter;
+}
