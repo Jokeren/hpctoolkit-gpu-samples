ac24b015611f5a8747ef6104c92ad625f7c66d0a
Jokeren <robinho364@gmail.com>
2019-05-17 20:37:01 -0700

    Update build script

--- Laghos/summit-build.sh
+++ Laghos/summit-build.sh
@@ -1,7 +1,6 @@
 # Tested for GCC >= 6.4.0
 export MPI_HOME=`echo $MPI_HOME`
 export CUDA_HOME=`echo $CUDA_HOME`
-export CPLUS_INCLUDE_PATH=`pwd`/cub-1.8.0:$CPLUS_INCLUDE_PATH
 
 # hypre
 wget https://computation.llnl.gov/projects/hypre-scalable-linear-solvers-multigrid-methods/download/hypre-2.11.2.tar.gz
@@ -28,24 +27,12 @@ make status
 make -j8
 cd ..
 
-# CUB
-wget https://github.com/NVlabs/cub/archive/cub-v1.8.0.tar.gz
-tar xf v1.8.0.tar.gz
-
-# MFEM with OCCA
-git clone git@github.com:mfem/mfem.git mfem-occa
-cd mfem-occa
-git checkout occa-dev
-make config MFEM_USE_MPI=YES HYPRE_DIR=`pwd`/../hypre-2.11.2/src/hypre MFEM_USE_METIS_5=YES METIS_DIR=`pwd`/../metis-5.1.0 MFEM_USE_OCCA=YES OCCA_DIR=`pwd`/../occa
-make status
-make -j8
-cd ..
-
 # raja
 git clone --recursive https://github.com/llnl/raja.git
 cd raja
 git checkout v0.5.0
 git submodule init && git submodule update
+rm -rf build
 mkdir build && cd build
 cmake -DENABLE_CUDA=TRUE -DCMAKE_CUDA_COMPILER=nvcc -DCUDA_ARCH=sm_70 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../../raja-install ..
 make install -j8
