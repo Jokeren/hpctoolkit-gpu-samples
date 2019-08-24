9d363ed214382649602f015149faa13836de5861
Jokeren <robinho364@gmail.com>
2019-08-23 23:00:39 -0500

    Add dummy functions

--- cu_multi_entries/vecSub.cu
+++ cu_multi_entries/vecSub.cu
@@ -1,5 +1,10 @@
 extern "C"
 __global__
+void dummy() {}
+
+
+extern "C"
+__global__
 void vecSub(int *l, int *r, int *p, size_t N, size_t iter) {
   size_t idx = blockDim.x * blockIdx.x + threadIdx.x;
   for (size_t i = 0; i < iter; ++i) {
