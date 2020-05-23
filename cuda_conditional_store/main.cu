651df720ee5825e58dfa0adc3664a9c7a872fe0d
Jokeren <robinho364@gmail.com>
2020-05-23 10:10:05 -0500

    Update

---
+++ cuda_conditional_store/main.cu
@@ -0,0 +1,107 @@
+#include <cstdio>
+#include <omp.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#ifdef USE_MPI
+#include <mpi.h>
+#endif
+
+#include "../utils/common.h"
+
+
+static const size_t N = 1000;
+
+
+void init(int *p, size_t size) {
+  for (size_t i = 0; i < size; ++i) {
+    p[i] = i;
+  }
+}
+
+
+void output(int *p, size_t size) {
+  for (size_t i = 0; i < size; ++i) {
+    printf("index %zu: %d\n", i, p[i]);
+  }
+}
+
+
+__global__
+void vecAdd(int *l, int *r, int *p, size_t N) {
+  size_t idx = blockDim.x * blockIdx.x + threadIdx.x;
+  int a;
+  if (idx < N) {
+    a = l[idx] + r[idx];
+  } else {
+    a = 1;
+  }
+  p[idx] += a;
+
+  int b;
+  if (idx < N) {
+    b = p[idx];
+  } else {
+    b = 2;
+  }
+  p[idx] += b;
+}
+
+
+int main(int argc, char *argv[]) {
+#ifdef USE_MPI
+  int numtasks, rank;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  printf("MPI task %d/%d\n", rank, numtasks);
+#endif
+
+  // Init device
+  int device_id = 0;
+  if (argc > 1) {
+    device_id = atoi(argv[1]);
+  }
+  cuda_init_device(device_id);
+
+  #pragma omp parallel
+  {
+    int l[N], r[N], p[N];
+    int *dl, *dr, *dp;
+
+    init(l, N);
+    init(r, N);
+
+    RUNTIME_API_CALL(cudaMalloc(&dl, N * sizeof(int)));
+    RUNTIME_API_CALL(cudaMalloc(&dr, N * sizeof(int)));
+    RUNTIME_API_CALL(cudaMalloc(&dp, N * sizeof(int)));
+
+    RUNTIME_API_CALL(cudaMemcpy(dl, l, N * sizeof(int), cudaMemcpyHostToDevice));
+    RUNTIME_API_CALL(cudaMemcpy(dr, r, N * sizeof(int), cudaMemcpyHostToDevice));
+
+    size_t threads = 256;
+    size_t blocks = (N - 1) / threads + 1;
+
+    GPU_TEST_FOR((vecAdd<<<blocks, threads>>>(dl, dr, dp, N)));
+
+    RUNTIME_API_CALL(cudaMemcpy(p, dp, N * sizeof(int), cudaMemcpyDeviceToHost));
+
+    RUNTIME_API_CALL(cudaFree(dl));
+    RUNTIME_API_CALL(cudaFree(dr));
+    RUNTIME_API_CALL(cudaFree(dp));
+
+#ifdef OUTPUT
+    #pragma omp critical
+    {
+      printf("Thread %d\n", omp_get_thread_num());
+      output(p, N);
+    }
+#endif
+  }
+
+  cudaDeviceSynchronize();
+
+#ifdef USE_MPI
+  MPI_Finalize();
+#endif
+  return 0;
+}
